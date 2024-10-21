0045c831005e99f17d7cd8535dfd1e9cc818d401
KawalGohil <kawalgohil@gmail.com>
2024-10-21 20:23:55 +0530

    Created background and working on buttons

--- app/src/main/java/com/example/habitgarden/Splash.kt
+++ app/src/main/java/com/example/habitgarden/Splash.kt
@@ -1,27 +1,20 @@
 package com.example.habitgarden
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        findViewById<Button>(R.id.new_Habit).setOnClickListener(){
-
-        }
-        findViewById<Button>(R.id.focus_Session).setOnClickListener(){
-
         }
     }
 }
